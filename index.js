2e78cf6865301e1a6228f2667714c091b3ddf725
Vyacheslav <parselian.study@yandex.ru>
2022-01-09 13:04:09 +0300

    updated wrangler.toml file

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const { WebClient } = require('@slack/web-api'),
 
 addEventListener('fetch', (event) => {
   try {
-    event.respondWith(handleEvent(event))
+    event.respondWith(sendMessage(event))
   } catch (error) {
     event.respondWith(new Response('Internal Error', { status: 500 }))
   }
 })
 
-async function handleEvent(event) {
-  console.log('success')
+async function sendMessage(channel, message) {
+  console.log('called');
+
+  if (new Date().getUTCHours() === 7 && new Date().getUTCMinutes() === 0) {
+    const client = new WebClient(TOKEN, {
+      logLevel: LogLevel.DEBUG,
+    });
+
+    console.log('worked');
+
+    client.chat.postMessage({
+      token: AUTH_TOKEN,
+      channel: `#${channel}`,
+      text: message,
+    });
+  }
 }
